f0ace4b7cb8265df849e2a041250929cfe1a5f3b
nick_hart <nhartmusic@gmail.com>
2023-12-27 17:23:32 -0800

    we good

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,18 @@
+
 import React from 'react'
 import ReactDOM from 'react-dom'
+//service worker
 import { unregister } from './core'
+//styles
+import { GlobalStyles } from './styles'
 
 
-ReactDOM.render(<div>Hello World</div>, document.getElementById('root'))
+ReactDOM.render(
+  <>
+    <GlobalStyles />
+    <div>Hello World</div>
+  </>, 
+  document.getElementById('root')
+)
 
 unregister()
